bc0883d6904da2f33f78930f17f4895c9b3cbc82
youshang_pc <20516411+youshang2580@users.noreply.github.com>
2023-06-15 23:08:17 +0800

    modified second commit~

--- src/main/java/top/x9t448g/git/TestGit.java
+++ src/main/java/top/x9t448g/git/TestGit.java
@@ -3,6 +3,6 @@ package top.x9t448g.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git ~");
-        System.out.println("hello git ~1");
+        System.out.println("hello git ~2");
     }
 }
